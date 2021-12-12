c34f0c9dacd936d4e88dfdb11ff626e99c7ce19c
Maxim Loginov <maxim.loginov@mail.ru>
2021-12-12 14:51:57 +0700

    simplified settings - DO NOT USE IN PRODUCTION
    Please see the original tutorial how to do this correctly
    https://dontrepeatyourself.org/post/django-blog-tutorial-part-5-deployment-on-heroku

--- blog/settings.py
+++ blog/settings.py
@@ -32,7 +32,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ["django-blog-rouizi.herokuapp.com"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -141,7 +141,7 @@ MEDIA_URL = '/media/'
 
 LOGIN_URL = "/users/login/"
 
-if os.environ.get('ENV') == 'PRODUCTION':
+if True:
     STATIC_ROOT = BASE_DIR / 'staticfiles'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     DATABASES['default'] = dj_database_url.config(
